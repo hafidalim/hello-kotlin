bad039e8415a16edc7ee6cb4b130a069486105de
hafidam <hafidd456@gmail.com>
2020-11-17 09:06:09 +0700

    chapter 2

--- src/chapter2.kt
+++ src/chapter2.kt
@@ -29,7 +29,56 @@ fun main() {
 
     println(bigString)
 
-    val firstName = "Hafid"
+    val firstName = "Hafid "
     val lastName = "Ali Mustaqim"
-    println("$firstName $lastName")
+    val fullName = firstName + lastName
+    val myDetails = "Halo nama saya $fullName"
+    println(myDetails)
+
+    val coordinates : Pair<Int, Int> = Pair(1,2)
+    println(coordinates)
+
+    //type inference
+    val coorInfered = Pair(10,9)
+
+    //to
+    val coorWithTo = 10 to 8
+
+    val x = coorWithTo.first
+    val y = coorWithTo.second
+    println(y)
+
+    //Triple
+    val coordinate3D = Triple(3,4,5)
+    val (x1,y1,z1) = coordinate3D
+
+    val tgl = Triple(10,2,1)
+    val (month,_,years) = tgl
+
+    val coordinateS = 2 to 3
+    val (row, column) = coordinateS
+
+    val date = Triple(15,8,2015)
+    val day = date.first
+
+    var name = "Joe"
+    name += " Howard"
+
+    val triple = Triple(100,1.5,10)
+    val value = triple.second
+
+    val number = 10
+    val muliplier = 5
+    val summary = "$number multiplied by $muliplier equals ${number*muliplier}"
+    println(summary)
+
+    val a = 4
+    val b : Short = 100
+    val c : Byte = 12
+
+    val itung = a + b - c
+    println(itung)
+
+
+
 }
